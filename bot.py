967f35643cdad671c8b696032d882d2738547122
santo-philip <santop7510@gmail.com>
2024-05-21 00:11:19 +0530

    added unmute  button

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from pyrogram import idle, Client
 
 api_id = 1474940
 api_hash = "779e8d2b32ef76d0b7a11fb5f132a6b6"
-bot_token = "5804042113:AAEgh4_QI_TLhXxvVNVGyMAcCStTcqSdZUo"
+bot_token = "6513923912:AAEN9ISrYV8-ivtS9BNaq0hSh7HC0SUkWDk"
 plugins = dict(root='plugins')
 
 app = Client(
