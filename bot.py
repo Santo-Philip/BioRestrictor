81c74789714c243529eabaf862ee41839934b9aa
Santo-Philip <santop7510@gmail.com>
2024-07-08 12:34:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from pyrogram import idle, Client
 
 api_id = 1474940
 api_hash = "779e8d2b32ef76d0b7a11fb5f132a6b6"
-bot_token = "6513923912:AAEN9ISrYV8-ivtS9BNaq0hSh7HC0SUkWDk"
+bot_token = "6513923912:AAHqD8O3NSIr2eZ27nqBt7uSKQfDHi4KgZk"
 plugins = dict(root='plugins')
 
 app = Client(
